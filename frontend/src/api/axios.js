eeefe03b89bededd596dd444b97cc8182a1171f1
Aditya Chauhan <adityachauhan.ind1@gmail.com>
2026-02-19 11:13:59 +0530

    Hotfix : axios link change

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -2,8 +2,8 @@
 import axios from 'axios';
 // Creates a custom axios client
 const api = axios.create({
-    // baseURL:  "https://metroflow-kjnk.onrender.com" || "http://localhost:5000",
-    baseURL:"http://localhost:5000",
+    baseURL:  "https://metroflow-kjnk.onrender.com" || "http://localhost:5000",
+    // baseURL:"http://localhost:5000",
     headers:{
         "Content-Type":"application/json",
     },
